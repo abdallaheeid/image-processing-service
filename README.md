398a2852393b5696dbd679339614f7b4a3ed89e9
Abdallah Eid <abdallaheeid@icloud.com>
2025-12-09 19:26:09 +0100

    Adding API Rate Limiting To the Project using Bucket4j

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 **Use Kafka in this project**
 
-Next step is connect this to AWS S3
+Next step is connect this to AWS S3 [done]
 
-And this streaming with kafka
+And this streaming with kafka [done]
+
+Next up: 
+    - Adding Caching using Redis
+    - Adding Rate Limit for user using Bucket4j
+
 
---
+++ src/main/java/org/abdallah/imageprocessingservice/registry/Bucket4jRateLimitApp.java
@@ -0,0 +1,20 @@
+package org.abdallah.imageprocessingservice.registry;
+
+import org.abdallah.imageprocessingservice.interceptors.RateLimitInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Bucket4jRateLimitApp implements WebMvcConfigurer {
+
+    @Autowired
+    private RateLimitInterceptor rateLimitInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(rateLimitInterceptor)
+                .addPathPatterns("/api/images/*/transform-async"); // YOUR ENDPOINT
+    }
+}
